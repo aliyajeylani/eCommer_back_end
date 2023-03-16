dda03a41f79c1b6fc823d2ce4df13c7d71854300
aliyajeylani <aliya.jeylani24@gmail.com>
2023-03-16 18:30:53 -0500

    Working on api routes

---
+++ config/connection.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+const Sequelize = require('sequelize');
+
+let sequelize;
+
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+
+  const sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: 'localhost',
+      dialect: 'mysql',
+      port: 3306,
+      dialectOptions: {
+        decimalNumbers: true,
+      },
+    })
+};
+
+module.exports = sequelize;
